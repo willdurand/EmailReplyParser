1bbc6930c0cca3875fadde6fffb67cc5c8437ce5
William DURAND <william.durand1@gmail.com>
2011-11-16 08:24:41 +0100

    [Tests] Fixed EmailReplyParserTest

--- tests/EmailReplyParser/Tests/EmailReplyParserTest.php
+++ tests/EmailReplyParser/Tests/EmailReplyParserTest.php
@@ -6,9 +6,18 @@ use EmailReplyParser\EmailReplyParser;
 
 class EmailReplyParserTest extends TestCase
 {
-    public function testRead()
+    public function testReadWithNullContent()
     {
         $reply = EmailReplyParser::read(null);
+
+        $this->assertTrue(is_array($reply));
+        $this->assertEquals(1, count($reply));
+        $this->assertEmpty($reply[0]->__toString());
+    }
+
+    public function testReadWithEmptyContent()
+    {
+        $reply = EmailReplyParser::read('');
 
         $this->assertTrue(is_array($reply));
         $this->assertEquals(1, count($reply));
